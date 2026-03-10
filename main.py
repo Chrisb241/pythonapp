1c8c6a1cf4a56e7fc9d0daff3c48171a098b2a88
Chris.B <chris.boupassia@esme.fr>
2026-03-10 15:50:18 +0100

    Add Python API program

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-CTRL + X
+import requests
+
+def main():
+    response = requests.get("https://api.github.com")
+    print("Status code:", response.status_code)
+    print("Headers:", response.headers["content-type"])
+
+if __name__ == "__main__":
+    main()
